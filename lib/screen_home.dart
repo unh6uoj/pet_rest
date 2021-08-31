17e3b72963a199cd6e4043fa622ec314f4590a32
dayoungYNC <68107013+dadayoung0@users.noreply.github.com>
2021-08-31 18:31:48 +0900

    Add files via upload

--- lib/screen_home.dart
+++ lib/screen_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +11,54 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(children: <Widget>[
+            Expanded(child: Image(image: AssetImage('images/pet_food.png'))),
+            Expanded(child: Image(image: AssetImage('images/pet_water.png')))
+          ]),
+          Row(children: <Widget>[
+            Expanded(
+                child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: new LinearPercentIndicator(
+                        animation: true,
+                        lineHeight: 20.0,
+                        animationDuration: 2000,
+                        percent: 0.9,
+                        center: Text("90.0%"),
+                        linearStrokeCap: LinearStrokeCap.roundAll,
+                        progressColor: Colors.greenAccent))),
+            Expanded(
+                child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: new LinearPercentIndicator(
+                        animation: true,
+                        lineHeight: 20.0,
+                        animationDuration: 2000,
+                        percent: 0.9,
+                        center: Text("90.0%"),
+                        linearStrokeCap: LinearStrokeCap.roundAll,
+                        progressColor: Colors.greenAccent)))
+          ]),
+          Row(children: <Widget>[
+            // percentage bar
+            Text("percentage bar")
+          ]),
+          Row(children: <Widget>[
+            // Buttons
+            Text("Buttons")
+          ]),
+          Row(children: <Widget>[
+            // move graph
+            Text("move graph")
+          ]),
+          Row(children: <Widget>[
+            // ball Button
+            Text("ball button")
+          ])
+        ]);
   }
 }

674a28b9c63729c8dd8541da4863600067865216
unh6uoj <fpdlxms159@gmail.com>
2021-10-13 12:33:58 +0900

    complete_log_logic

--- lib/screen_log.dart
+++ lib/screen_log.dart
@@ -41,24 +41,69 @@ class HistoryListView extends StatefulWidget {
 }
 
 class _HistoryListViewState extends State<HistoryListView> {
-  List<Widget> histRows = [];
+  // sqlite에서 받아온 데이터 저장
   List<History> histDatas = [];
+
+  // 각 데이터들이 들어갈 Row위젯
+  // 하단의 Box가 생성될 때 초기화
+  List<Widget> histRows = [];
+
+  // histRow가 포함되는 위젯
+  // 일일별로 하나의 Box
+  List<Widget> histBoxs = [];
+
+  // 현재 가져오고 있는 날짜 데이터
+  String curDate = '';
 
   @override
   void initState() {
     super.initState();
 
-    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
-      Provider.of<LogProvider>(context, listen: false)
-          .getAllData()
-          .then((value) => setState(() {
-                histRows = [];
-                histDatas = value;
-              }));
-    });
+    Provider.of<LogProvider>(context, listen: false)
+        .getAllData()
+        .then((value) => setState(() {
+              histRows = [];
+              histDatas = value;
+              curDate = value[0].date;
+
+              addBoxHeader(curDate);
+
+              // 가져온 데이터들을 나눠주는 부분
+              for (int i = 0; i < histDatas.length; i++) {
+                if (histDatas[i].date != curDate) {
+                  // 리스트에 그냥 add하면 setState가 호출되지 않는다.
+                  // 이렇게 새로운 리스트를 생성 해야한다.
+                  // ...은 리스트의 모든 요소를 가져온다
+                  histBoxs = [...histBoxs, (HistoryBox(histRowList: histRows))];
+                  histRows = [];
+
+                  addBoxHeader(histDatas[i].date);
+
+                  curDate = histDatas[i].date;
+                }
+
+                histRows.add(Row(
+                  children: <Widget>[
+                    // Expanded(
+                    //   child: Text(histDatas[i].date),
+                    // ),
+                    Expanded(
+                      child: Text(histDatas[i].activity),
+                    )
+                  ],
+                ));
+
+                if (i == histDatas.length - 1) {
+                  histBoxs.add(HistoryBox(histRowList: histRows));
+                }
+              }
+            }));
+
+    // 위젯이 만들어지면 실행됨
+    // WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {});
   }
 
-  void addCardHeader(String date) {
+  void addBoxHeader(String date) {
     // 문자열 나누기
     List splitedDate = date.split(" ");
 
@@ -82,39 +127,7 @@ class _HistoryListViewState extends State<HistoryListView> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> histCards = [];
-    String curDate = histDatas[0].date;
-
-    histRows = [];
-
-    addCardHeader(curDate);
-
-    for (int i = 0; i < histDatas.length; i++) {
-      if (histDatas[i].date != curDate) {
-        histCards.add(HistoryBox(histRowList: histRows));
-        histRows = [];
-
-        addCardHeader(histDatas[i].date);
-
-        curDate = histDatas[i].date;
-      }
-
-      histRows.add(Row(
-        children: <Widget>[
-          // Expanded(
-          //   child: Text(histDatas[i].date),
-          // ),
-          Expanded(
-            child: Text(histDatas[i].activity),
-          )
-        ],
-      ));
-
-      if (i == histDatas.length - 1) {
-        histCards.add(HistoryBox(histRowList: histRows));
-      }
-    }
-    return ListView(children: histCards);
+    return ListView(children: histBoxs);
   }
 }
 
